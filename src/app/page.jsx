bdaac72dc3906b9254d94743c901fde042da2420
WalidurTanjim <mohammadtanjim91@gmail.com>
2026-06-25 19:51:54 +0600

    next-app l1b12m69 first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-full px-2 md:px-5 lg:px-8 py-10">
+      <h1 className="text-center text-2xl text-slate-700 font-medium">Next App</h1>
+    </div>
+  )
+}
